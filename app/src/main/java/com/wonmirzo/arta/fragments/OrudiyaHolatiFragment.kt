f429c078dbd8b7d2cb90ddc5126c2cf9916232d5
Mirzohid Dilshodov <md20021203@gmail.com>
2022-10-14 18:52:44 +0500

    update

--- app/src/main/java/com/wonmirzo/arta/fragments/OrudiyaHolatiFragment.kt
+++ app/src/main/java/com/wonmirzo/arta/fragments/OrudiyaHolatiFragment.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.wonmirzo.arta.R
 import com.wonmirzo.arta.databinding.FragmentOrudiyaHolatiBinding
 
 /**
@@ -35,6 +40,38 @@ class OrudiyaHolatiFragment : BaseFragment() {
     }
 
     private fun initViews() {
+        setSpinnerOrudiyaTuri()
+    }
 
+    private fun setSpinnerOrudiyaTuri() {
+        val orudiyaTurlari = resources.getStringArray(R.array.spinner_orudiya_turi)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_orudiya_turi,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerOrudiyaTuri.adapter = adapter
+        }
+
+        binding.spinnerOrudiyaTuri.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    Toast.makeText(
+                        requireContext(),
+                        orudiyaTurlari[position],
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
     }
 }
